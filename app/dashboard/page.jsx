0e102fb2b0a847c165530d67386f7ca35245fb67
Gowdaman p <56969439+gowdaman-dev@users.noreply.github.com>
2023-11-30 13:57:42 +0530

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,13 +4,8 @@ import { useSession } from "next-auth/react";
 import DashboardNav from "../components/DashboardNav";
 import AddNotice from "../components/AddNotice";
 import UserNotice from "../components/UserNotice";
-import { useRouter } from "next/navigation";
 function page() {
   const { data: session } = useSession();
-  const router = useRouter()
-  if (!session?.user?.email){
-    router.push('/login')
-  }
   return (
     <div className="flex flex-col w-screen min-h-screen text-white bg-black ">
       <DashboardNav/>
