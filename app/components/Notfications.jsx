e1d0c72847360c1e133c56402a242fff7ae67500
Gowdaman p <56969439+gowdaman-dev@users.noreply.github.com>
2023-12-05 17:46:40 +0530

    Update Notfications.jsx

--- app/components/Notfications.jsx
+++ app/components/Notfications.jsx
@@ -38,6 +38,7 @@ function Notfications() {
         console.log("Empty or invalid data received");
       }
       let utterance = new SpeechSynthesisUtterance(message);
+      utterance.rate = 8
       speechSynthesis.speak(utterance);
       return setLimit(data.length)
     }
