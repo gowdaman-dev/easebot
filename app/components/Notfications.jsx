2bd06b865a7853f47fc9777303557361e68911c8
Gowdaman p <56969439+gowdaman-dev@users.noreply.github.com>
2023-12-05 18:19:58 +0530

    Update Notfications.jsx

--- app/components/Notfications.jsx
+++ app/components/Notfications.jsx
@@ -39,7 +39,7 @@ function Notfications() {
       }
       let utterance = new SpeechSynthesisUtterance(message);
       utterance.rate = .7
-      utterance.voice = speechSynthesis.getVoices()[1450]
+      utterance.voice = speechSynthesis.getVoices()[1480]
       speechSynthesis.speak(utterance);
       return setLimit(data.length)
     }
